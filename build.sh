2f1abdf4b998deb7bf9797901b4e0e270fc82efe
0x1 <104192394+zeroXone-github@users.noreply.github.com>
2024-07-22 19:40:16 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ FINAL_KERNEL_BUILD_PARA="ARCH=$TARGET_ARCH \
 TARGET_KERNEL_FILE=arch/arm64/boot/Image;
 TARGET_KERNEL_DTB=arch/arm64/boot/dtb;
 TARGET_KERNEL_DTBO=arch/arm64/boot/dtbo.img
-TARGET_KERNEL_NAME=KzeroXone-kernel-MIUI-HyperOS;
+TARGET_KERNEL_NAME=zeroXone-kernel-MIUI-HyperOS;
 TARGET_KERNEL_MOD_VERSION=$(make kernelversion)
 
 ANYKERNEL_PATH=anykernel
